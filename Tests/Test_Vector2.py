c5723ebb4588efd0614fdcb381d21abca8db2f4b
Zakary Bruce-Kyle <zb201421@falmouth.ac.uk>
2019-02-07 13:39:57 +0000

    Added Vector2 class and testing out travis.

---
+++ Tests/Test_Vector2.py
@@ -0,0 +1,25 @@
+from .. import Vector2
+
+# Vectors can be created
+assert Vector2(1,1) != None
+
+# Create test vectors
+test_vec1 = Vector2(1,1)
+test_vec2 = Vector2(5,50)
+
+# Vectors can be read
+assert test_vec1.x == 1
+assert test_vec2.y == 1
+
+# Vectors can be added
+assert (test_vec1 + test_vec2) == Vector(6, 51)
+
+# Vectors can be subtracted
+assert (test_vec1 + test_vec2) == Vector(4, 49)
+
+# Vectors can be compared
+assert (test_vec1 == test_vec2) == False
+assert (test_vec1 != test_vec2) == True
+
+# Vectors can be printed
+assert str(test_vec1) == "x: 1 y: 1" 
---
+++ Vector2.py
@@ -0,0 +1,28 @@
+class Vector2:
+
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __add__(self, other_vector2):
+        return Vector2(self.x + other_vector2.x, self.y + other_vector2.y)
+
+    def __sub__(self, other_vector2):
+        return Vector2(self.x - other_vector2.x, self.y - other_vector2.y)
+
+    def __eq__(self, other_vector2):
+        return self.x == other_vector2.x and self.y == other_vector2.y
+
+    def __ne__(self, other_vector2):
+        return not (self == other_vector2)
+
+    def __getitem__(self, index):
+        if index == 0:
+            return self.x
+        elif index == 1:
+            return self.y
+        else:
+            return None
+
+    def __str__(self):
+        return "x: " + str(self.x) + " y: " + str(self.y)
